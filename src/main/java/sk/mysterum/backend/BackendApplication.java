7fa7d3d0bfe1ca7fc1e366a364b64bf4535bd1f7
MagSG <jtulek@at-rt.com>
2020-11-21 10:07:56 +0100

    Functional backend for getting text based on text files on desktop

--- src/main/java/sk/mysterum/backend/BackendApplication.java
+++ src/main/java/sk/mysterum/backend/BackendApplication.java
@@ -2,17 +2,22 @@ package sk.mysterum.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import sk.mysterum.backend.services.PrintLogo;
+import sk.mysterum.backend.uselessstuff.PrintLogo;
 
 @SpringBootApplication
 public class BackendApplication {
 	private final static PrintLogo lgo = new PrintLogo();
+	private static boolean isFirstTime = true;
 
 	public static void main(String[] args){
 
 
+		if (isFirstTime){
+			lgo.print();
+			isFirstTime = false;
+		}
 		SpringApplication.run(BackendApplication.class, args);
-		lgo.print();
+
 
 	}
 
