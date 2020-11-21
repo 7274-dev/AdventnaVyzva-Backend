2a9696487a3db9e118ec71da864e5966d9ff1f0e
MagSG <jtulek@at-rt.com>
2020-11-21 10:35:22 +0100

    Resolved error with printing our logo

--- src/main/java/sk/mysterum/backend/BackendApplication.java
+++ src/main/java/sk/mysterum/backend/BackendApplication.java
@@ -2,18 +2,43 @@ package sk.mysterum.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import sk.mysterum.backend.uselessstuff.PrintLogo;
 
 @SpringBootApplication
 public class BackendApplication {
-	private final static PrintLogo lgo = new PrintLogo();
+
 	private static boolean isFirstTime = true;
 
 	public static void main(String[] args){
 
 
 		if (isFirstTime){
-			lgo.print();
+
+			//****************LOGOOOOOO*******************//
+			System.out.println("\n" +
+					"\n" +
+					"77777777777777777777 222222222222222    77777777777777777777   444444444  \n" +
+					"7::::::::::::::::::72:::::::::::::::22  7::::::::::::::::::7  4::::::::4  \n" +
+					"7::::::::::::::::::72::::::222222:::::2 7::::::::::::::::::7 4:::::::::4  \n" +
+					"777777777777:::::::72222222     2:::::2 777777777777:::::::74::::44::::4  \n" +
+					"           7::::::7             2:::::2            7::::::74::::4 4::::4  \n" +
+					"          7::::::7              2:::::2           7::::::74::::4  4::::4  \n" +
+					"         7::::::7            2222::::2           7::::::74::::4   4::::4  \n" +
+					"        7::::::7        22222::::::22           7::::::74::::444444::::444\n" +
+					"       7::::::7       22::::::::222            7::::::7 4::::::::::::::::4\n" +
+					"      7::::::7       2:::::22222              7::::::7  4444444444:::::444\n" +
+					"     7::::::7       2:::::2                  7::::::7             4::::4  \n" +
+					"    7::::::7        2:::::2                 7::::::7              4::::4  \n" +
+					"   7::::::7         2:::::2       222222   7::::::7               4::::4  \n" +
+					"  7::::::7          2::::::2222222:::::2  7::::::7              44::::::44\n" +
+					" 7::::::7           2::::::::::::::::::2 7::::::7               4::::::::4\n" +
+					"77777777            2222222222222222222277777777                4444444444\n" +
+					"\n" +
+					" _____   _______ _    _ \n" +
+					"(____ \\ (_______) |  | |\n" +
+					" _   \\ \\ _____  | |  | |\n" +
+					"| |   | |  ___)  \\ \\/ / \n" +
+					"| |__/ /| |_____  \\  /  \n" +
+					"|_____/ |_______)  \\/  ");
 			isFirstTime = false;
 		}
 		SpringApplication.run(BackendApplication.class, args);
