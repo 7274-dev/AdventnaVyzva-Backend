2aa918a1777642d4fe1855ce21598aa04dfc11ac
MagSG <jtulek@at-rt.com>
2020-11-21 12:50:21 +0100

    Repairing remove from database feature, maybe fix?

--- src/main/java/sk/mysterum/backend/repositories/TextRepository.java
+++ src/main/java/sk/mysterum/backend/repositories/TextRepository.java
@@ -3,9 +3,11 @@ package sk.mysterum.backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sk.mysterum.backend.model.TextModel;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface TextRepository extends JpaRepository<TextModel, Long> {
     public List<TextModel> findByDay(Integer day);
+    @Transactional
     public void deleteAllByDayEquals(Integer day);
 }
