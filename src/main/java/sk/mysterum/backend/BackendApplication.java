afa213d4a82daafac66dcada763fe05bc052f6b9
MagSG <jtulek@at-rt.com>
2020-11-24 13:28:11 +0100

    Testing out self destruct capability Pushing to get the code to linux

--- src/main/java/sk/mysterum/backend/BackendApplication.java
+++ src/main/java/sk/mysterum/backend/BackendApplication.java
@@ -1,11 +1,14 @@
 package sk.mysterum.backend;
 
+import org.aspectj.weaver.ast.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import sk.mysterum.backend.services.MailService;
 
 import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 @EnableJpaRepositories
 @SpringBootApplication
@@ -14,7 +17,7 @@ public class BackendApplication {
 	private static boolean isFirstTime = true;
 
 
-	public static void main(String[] args) throws MessagingException {
+	public static void main(String[] args) throws MessagingException, UnsupportedEncodingException {
 
 
 		if (isFirstTime){
@@ -48,7 +51,9 @@ public class BackendApplication {
 			isFirstTime = false;
 		}
 		//SpringApplication.run(BackendApplication.class, args);
-		
+		String path = Test.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		String decodedPath = URLDecoder.decode(path,"UTF-8");
+		System.out.println(decodedPath);
 
 	}
 
